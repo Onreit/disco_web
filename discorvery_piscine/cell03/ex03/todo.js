ea742e3e900c9f84014293663043f44be852787d
Onreit <50305647+Onreit@users.noreply.github.com>
2022-04-29 15:17:58 +0200

    Add files via upload

--- discorvery_piscine/cell03/ex03/todo.js
+++ discorvery_piscine/cell03/ex03/todo.js
@@ -1,24 +1,90 @@
-// detect finish loading
-// search button
-// agir sur ton bouton
-
 var button = document.getElementById("bouton");
 
 var myNodelist = document.getElementsByTagName("DIV");
+
 var i;
-var myFunction = function () {
+
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+function setCookie(cname, cvalue, exdays) {
+  const d = new Date();
+  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  let expires = "expires="+ d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for(let i = 0; i <ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+function getListOfId() {
+  console.log(document.cookie)
+  return getCookie("listofid").split(",");
+}
+
+function setListOfId(cname) {
+  var cookielist = getCookie("listofid")
+  cookielist = cookielist.split(",");
+  cookielist.push(cname);
+  setCookie("listofid",cookielist.join(","))
+}
+
+
+function createTaskDiv (name) {
+  var div = document.createElement("div");
+  var t = document.createTextNode(name);
+  div.appendChild(t);
+  document.getElementById("ft_list").appendChild(div)
+  div.onclick = function () {
+    if (confirm("Voulez vous supprimer cette tache ?"))
+      document.getElementById("ft_list").removeChild(div);
+    
+  }
+  return div;
+}
+
+function createTask () {
   let task = prompt("Tache à ajouter");
   console.log(task);
   if (task === '')
     return;
-  var div = document.createElement("div");
-  var t = document.createTextNode("- " + task);
-  div.appendChild(t);
-  document.getElementById("ft_list").appendChild(div)
-  document.cookie = "username=" + task + "; path=/"
-  div.onclick = function () {
-    document.getElementById("ft_list").removeChild(div)
-    document.cookie = "username="
+  
+  const div = createTaskDiv(task);
+  div.id = getRandomColor()
+
+  // setCookie(div.id, task, 1000)
+  setListOfId(div.id)
+  console.log(document.cookie)
+}
+button.addEventListener("click", createTask);
+
+function listTaskCookie() {
+  let listTask = getListOfId()
+
+  if ( listTask !== null ){
+      for(let i = 0; i < listTask.length; i++) {
+        createTaskDiv( getCookie(listTask[i]) );
+      }
   }
 }
-button.addEventListener("click", myFunction);
+
+window.onload =listTaskCookie;
